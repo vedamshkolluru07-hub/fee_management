0784907cd7b2618a26d83cac8d719b8b6502aa7e
vedamshkolluru07-hub <vedamshkolluru07@gmail.com>
2026-09-02 13:04:28 +0530

    allowed origins updated

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,8 +2,7 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://127.0.0.1:4200',
   'http://ssmodelhighschool.org',
-  // ⚠️ TEMP: no SSL yet. Once you get a cert, replace the line above with:
-  // 'https://ssmodelhighschool.org'
-];
+  'https://fee-management-black-three.vercel.app',
+];;
 
 module.exports = allowedOrigins;
