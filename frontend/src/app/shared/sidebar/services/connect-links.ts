3980a7ae3ba92f508790465d8be0b9f9d83ceb34
vedamshkolluru07-hub <vedamshkolluru07@gmail.com>
2026-09-02 13:41:32 +0530

    updated imports in service files frontend

--- frontend/src/app/shared/sidebar/services/connect-links.ts
+++ frontend/src/app/shared/sidebar/services/connect-links.ts
@@ -3,7 +3,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 import { ApiResponse, ConnectLinkAdmin, ConnectPlatform, SiteTheme } from '../../../core/services/website';
 
 @Injectable({ providedIn: 'root' })
